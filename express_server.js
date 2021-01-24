f46d3d6188e01396ba6db82aae4e0ca8708939bb
Jacob Hein <jacobhein1892@gmail.com>
2021-01-24 23:42:02 +0000

    added some login to start storing values the player picks

--- express_server.js
+++ express_server.js
@@ -3,6 +3,27 @@ const app = express();
 const PORT = 8080; 
 app.set("view engine", "ejs");
 
+//Setting Body Parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// Helper
+function generateRandomString() {
+  return Math.floor((1 + Math.random()) * 0x100000).toString(16);
+}
+
+// Databases: 
+
+let ShipSpots = {
+  carrier: [],
+  battleship: [],
+  cruiser: [],
+  submarine: [],
+  destroyer: []
+}; 
+
 app.get('/login', (req, res) => {
   res.render('login');
 });
@@ -16,7 +37,20 @@ app.get('/home', (req, res) => [
   res.render('home')
 ]);
 app.post('/game', (req, res) => {
+  let gameNum = generateRandomString()
+  console.log(req.body.carrierStart)
+  ShipSpots[gameNum] = {
+    carrier: [req.body.carrierStart, req.body.carrierFinish],
+    battleship: [],
+    cruiser: [],
+    submarine: [],
+    destroyer: []
+  }
+  console.log(ShipSpots[gameNum])
   res.render('game')
+});
+app.get('/game', (req,res) => {
+  console.log(req.body)
 })
 
 
